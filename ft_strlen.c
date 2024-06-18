418686d34d09b65b96f8fc103aefaf5006ee8c08
Leonard Glauch <lglauch@3-B-9.42heilbronn.de>
2024-06-18 15:41:05 +0200

    .

--- ft_strlen.c
+++ ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: lglauch <lglauch@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/12 15:50:08 by lglauch           #+#    #+#             */
-/*   Updated: 2023/10/17 16:31:30 by lglauch          ###   ########.fr       */
+/*   Updated: 2024/06/18 15:40:45 by lglauch          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	i;
 
+	if (!s)
+		return (0);
 	i = 0;
 	if (s[i] != 0)
 		while (s[i] != 0)
